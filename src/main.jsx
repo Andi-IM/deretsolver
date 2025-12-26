ab7e6cd7e71ced6355e129c9a49d6f3c03ba9046
Andi-IM <andi.irhamm@gmail.com>
2025-12-26 20:35:45 +0700

    feat: Initialize React application with core styling, routing, and essential providers.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import '@fontsource/material-symbols-outlined';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 import { HelmetProvider } from 'react-helmet-async';
