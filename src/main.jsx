c0c24c358697d8719f7023a82138708ecbbc168b
Andi-IM <andi.irhamm@gmail.com>
2025-12-26 14:00:24 +0700

    feat: Initialize React application with reCAPTCHA v3 and add Firebase Hosting CI/CD workflow.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import 'material-symbols/outlined.css';
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "material-symbols/outlined.css";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <GoogleReCaptchaProvider reCaptchaKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}>
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  </GoogleReCaptchaProvider>
+);
